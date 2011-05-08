a7ae0cfde0dc5206b84c3980c2942337b2cc35fd
loris <loris@kant.(none)>
2011-05-08 22:10:50 +0200

    lexer rules wrote

---
+++ lexer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import ply.lex as lex
+from ply.lex import LexError
+
+tokens = [
+    'VARIABLE',
+    'LPAREN',
+    'RPAREN'
+    ]
+
+literals = ['(', ')', '\\', '.']
+
+def t_VARIABLE (t):
+    r'[a-z]'
+    return t
+
+if __name__ == "__main__":
+    lexer = lex.lex ()
+    
+    while True:
+        try: s = raw_input ('>>>')
+        except EOFError: break
+        lexer.input (s)
+
+        while True:
+            try: tok = lexer.token()
+            except LexError: 
+                print "Illegal expression"
+                break
+            if not tok: break
+            print tok
+        
+
