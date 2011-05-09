d016bdde01f92ab87aa07fa5469b8776ca045db1
loris <loris@kant.(none)>
2011-05-09 14:54:05 +0200

    added substitution and beta reduction

--- parser.py
+++ parser.py
@@ -14,21 +14,31 @@ class Variable:
     def __init__ (self, uName):
         self._name = uName
 
+    def __str__ (self):
+        return str (self._name)
+
 class Application:
     def __init__ (self, uFirst, uSecond):
         self._first = uFirst
         self._second = uSecond
+        
+    def __str__ (self):
+        return '(' + str (self._first) + ')' + '(' + str (self._second) + ')'
 
 class Abstraction:
     def __init__ (self, uVariable, uBody):
         self._variable = uVariable
         self._body = uBody
 
+    def __str__ (self):
+        return '\\' + str (self._variable) + '.' + str (self._body)
+
 def p_start (p):
     ''' Program : Term '''
     print "Bound Variables are: ", bound_vars (p[1])
     print "Free Variables are: ", free_vars (p[1])
-
+    print beta_reduce (p[1])
+    
 
 def p_paren (p):
     ''' Term : '(' Term ')' '''
@@ -50,18 +60,54 @@ def p_abstraction (p):
 def bound_vars (uTerm):
     if isinstance (uTerm, Variable):
         return set([])
+
     elif isinstance (uTerm, Application):
         return bound_vars (uTerm._first).union (bound_vars (uTerm._second))
+
     elif isinstance (uTerm, Abstraction):
         return set ([uTerm._variable]).union (bound_vars (uTerm._body))
 
 def free_vars (uTerm):
     if isinstance (uTerm, Variable):
         return set([uTerm._name])
+
     elif isinstance (uTerm, Application):
         return free_vars (uTerm._first).union (free_vars (uTerm._second))
+
     elif isinstance (uTerm, Abstraction):
         return free_vars (uTerm._body).difference (uTerm._variable)
+
+def substitute (uTerm, uToSubstitute, uNewTerm):
+    if isinstance (uTerm, Variable):
+        if uTerm._name == uToSubstitute:
+            return uNewTerm
+
+    elif isinstance (uTerm, Application):
+        uTerm._first = substitute (uTerm._first, uToSubstitute, uNewTerm)
+        uTerm._second = substitute (uTerm._second, uToSubstitute, uNewTerm)
+
+    elif isinstance (uTerm, Abstraction):
+        if uTerm._variable == uToSubstitute:
+            uTerm._body = substitute (uTerm._body, uToSubstitute, uNewTerm)
+
+    return uTerm
+
+def beta_reduce (uTerm):
+    if isinstance (uTerm, Variable):
+        return uTerm
+
+    elif isinstance (uTerm, Abstraction):
+        return uTerm
+
+    elif isinstance (uTerm, Application):
+        uTerm._first = beta_reduce (uTerm._first)
+        uTerm._second = beta_reduce (uTerm._second)
+        
+        if isinstance (uTerm._first, Abstraction):
+            return beta_reduce (substitute (uTerm._first._body,
+                                            uTerm._first._variable,
+                                            uTerm._second))
+        else: return uTerm
         
         
 if __name__ == "__main__":
