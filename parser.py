beac4be91f29b6eb4e481e5a7c27d8374a7765c0
loris <loris@kant.(none)>
2011-05-13 18:33:53 +0200

    fixed issue in multi-step reduction. fixed leftmost-outermost redex search. towards adding of deep copy of lambda terms.

--- parser.py
+++ parser.py
@@ -57,9 +57,9 @@ class Abstraction:
 
 def p_start (p):
     ''' Program : Term '''
-    print "Bound Variables are: ", bound_vars (p[1])
-    print "Free Variables are: ", free_vars (p[1])
-    print beta_reduce (p[1])
+    # print "Bound Variables are: ", bound_vars (p[1])
+    # print "Free Variables are: ", free_vars (p[1])
+    print multi_step_beta_reduce (p[1])
     
 
 def p_paren (p):
@@ -89,6 +89,7 @@ def bound_vars (uTerm):
     elif isinstance (uTerm, Abstraction):
         return set ([uTerm._variable]).union (bound_vars (uTerm._body))
 
+
 def free_vars (uTerm):
     if isinstance (uTerm, Variable):
         return set([uTerm._name])
@@ -98,6 +99,7 @@ def free_vars (uTerm):
 
     elif isinstance (uTerm, Abstraction):
         return free_vars (uTerm._body).difference (uTerm._variable)
+
 
 def substitute (uTerm, uToSubstitute, uNewTerm):
     if isinstance (uTerm, Variable):
@@ -114,6 +116,7 @@ def substitute (uTerm, uToSubstitute, uNewTerm):
 
     return uTerm
 
+
 def beta_reduce (uTerm):
     if isinstance (uTerm, Variable):
         return uTerm
@@ -123,24 +126,46 @@ def beta_reduce (uTerm):
 
     ## leftmost-outermost choice of redex
     elif isinstance (uTerm, Application):
+        ## outermost
         if isinstance (uTerm._first, Abstraction):
             return substitute (uTerm._first._body,
                                uTerm._first._variable,
                                uTerm._second)
         else:
-            uTerm._first = beta_reduce (uTerm._first)
-            # uTerm._second = beta_reduce (uTerm._second)
-            return uTerm
+            ## leftmost
+            new_first = beta_reduce (uTerm._first)
+            ##print new_first, " ", uTerm._first
+            if new_first != uTerm._first: 
+                uTerm._first = new_first
+                return uTerm
+            else: 
+                uTerm._second = beta_reduce (uTerm._second)
+                print uTerm._second
+                return uTerm
+            
+
 
 def multi_step_beta_reduce (uTerm):
     if isinstance (uTerm, Variable) or isinstance (uTerm, Abstraction):
         return uTerm
 
     elif isinstance (uTerm, Application):
-        t = beta_reduce (uTerm)
+        t = uTerm
+        reductions = []
+
         while (True):
+            t_str = str(t)
             new_t = beta_reduce (t)
-            if t == new_t: return t
+            new_t_str = str(new_t)
+
+            if t_str == new_t_str:
+                for b, a in reductions:
+                    print b, " -> ", a
+                return t
+
+            else:
+                reductions.append ((t_str, new_t_str))
+                t = new_t
         
 if __name__ == "__main__":
     lexer = lex.lex ()
