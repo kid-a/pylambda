5979f35cf74cc68b70ca0040b2d3c54b9df61612
Loris Fichera <loris.fichera@gmail.com>
2012-02-18 00:22:55 +0100

    not working

--- pylambda.py
+++ pylambda.py
@@ -9,42 +9,44 @@ from utilities import *
 
 from operations import bound_vars
 
-l = """
-                      o                  o         o
- ##                   O                  O         O
-#  #                  o                  O         o
-#  #                  O                  o         o
-   #      .oOo. O   o o  .oOoO' `oOOoOO. OoOo. .oOoO  .oOoO'
-   #      O   o o   O O  O   o   O  o  o O   o o   O  O   o
-   ##     o   O O   o o  o   O   o  O  O o   O O   o  o   O
-  ###     oOoO' `OoOO Oo `OoO'o  O  o  o `OoO' `OoO'o `OoO'o
-  # #     O         o
- #  #     o'     OoO'                                        
- #   ##
+# import cmd
 
-Welcome to pylambda 0.01 (2011-11-23) <http://github.com/kid-a/pylambda>
-Press CTRL+D to exit
-"""
+# lexer = None
+# parser = None
+
+# class PLCommandLine (cmd.Cmd):
+#     """Command line processor for PyLambda."""
+#     def __init__(self, *args, **kwargs):
+#         cmd.Cmd.__init__ (self)
+#         self.prompt = '>>>'
+#         self.intro = \
+# """
+#                       o                  o         o
+#  ##                   O                  O         O
+# #  #                  o                  O         o
+# #  #                  O                  o         o
+#    #      .oOo. O   o o  .oOoO' `oOOoOO. OoOo. .oOoO  .oOoO'
+#    #      O   o o   O O  O   o   O  o  o O   o o   O  O   o
+#    ##     o   O O   o o  o   O   o  O  O o   O O   o  o   O
+#   ###     oOoO' `OoOO Oo `OoO'o  O  o  o `OoO' `OoO'o `OoO'o
+#   # #     O         o
+#  #  #     o'     OoO'                                        
+#  #   ##
+
+# Welcome to pylambda 0.01 (2011-11-23) <http://github.com/kid-a/pylambda>
+# Press CTRL+D to exit
+# """
+        
+#     def default (self, s):
+#         term = parser.parse (s)
+#         print term
+#         print "Bound Variables are: ", bound_vars (term)
 
 
 if __name__ == "__main__":
-    print l
-
     lexer = lex.lex ()
     parser = yacc.yacc ()
-    history = CommandHistory ()
-
-    while True:
-        try:
-            s = raw_input ('>>>')
-            if s == "": continue
-
-            history.add (s)
-            term = parser.parse (s)
-
-            ## actual computation
-            print "Bound Variables are: ", bound_vars (term)
-
-        except EOFError:
-            #print "\nExiting. Bye!"
-            break
+    #print (parser.productions)
+    print parser.parse ('\\x.x')
+    print parser.parse ('a')
+    #PLCommandLine ().cmdloop ()
